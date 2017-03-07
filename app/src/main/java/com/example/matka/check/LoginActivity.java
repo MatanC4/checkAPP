22991de0d8b8209e8454a453e5eed8fcb959d2a7
matka <matanc4@mail.afeka.ac.il>
2017-03-07 23:26:06 +0200

    Facebook connect button added to login screen

--- app/src/main/java/com/example/matka/check/LoginActivity.java
+++ app/src/main/java/com/example/matka/check/LoginActivity.java
@@ -1,13 +1,53 @@
 package com.example.matka.check;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton =  (LoginButton)findViewById(R.id.fb_login_id);
+        textview = (TextView)findViewById(R.id.text_view);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textview.setText("Success + /n" +
+                        loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textview.setText("cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                textview.setText("Error occured");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 }
