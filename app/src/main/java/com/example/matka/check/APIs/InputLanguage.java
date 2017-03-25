9b12cb459ad9572b552bcb832f727ca22da13364
matka <matanc4@mail.afeka.ac.il>
2017-03-25 10:39:06 +0300

    MOVIE DB API Integration phase 1 - show popular

---
+++ app/src/main/java/com/example/matka/check/APIs/InputLanguage.java
@@ -0,0 +1,7 @@
+package com.example.matka.check.APIs;
+
+/**
+ * Created by matka on 21/03/17.
+ */
+public enum InputLanguage {
+}
--- app/src/main/java/com/example/matka/check/Category/CategoryActivity.java
+++ app/src/main/java/com/example/matka/check/Category/CategoryActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
 import com.example.matka.check.MainScreen.CategoriesListView;
 import com.example.matka.check.MainScreen.MainScreenCollectionPagerAdapter;
 import com.example.matka.check.MainScreen.UpNextListView;
